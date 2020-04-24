10e95921b84d01c7360aa76de4e8c904c0f27553
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-04-24 18:22:45 +0900

    typescriptの基本文法について

--- index.js
+++ index.js
@@ -1,2 +1,85 @@
-var hello = "hello";
-console.log(hello);
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = "hello";
+var double = "hello";
+var person = {
+    name: {
+        first: "Jack",
+        last: "Swith"
+    },
+    age: 21
+};
+var fruits = ["Apple", "Banana", "Grape"];
+var book = ["business", 1500, false];
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize[CoffeeSize["SHORT"] = 0] = "SHORT";
+    CoffeeSize[CoffeeSize["TALL"] = 1] = "TALL";
+    CoffeeSize[CoffeeSize["GRANDE"] = 2] = "GRANDE";
+    CoffeeSize[CoffeeSize["VENTI"] = 3] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
+};
+coffee.size = CoffeeSize.TALL;
+console.log(CoffeeSize.TALL);
+var anything = true;
+anything = "hello ";
+anything = ["hello", 33, true];
+anything = {};
+anything.fifififi = "fufurfhr";
+var banana = "banana";
+banana = anything;
+var unionType = 10;
+unionType = "hello";
+unionType.toUpperCase();
+var unionTypes = [21, "hello"];
+var apple = "apple";
+var clothSize = "medium";
+var cloth = {
+    color: "white",
+    size: "medium"
+};
+cloth.size = "large";
+var cloths = {
+    color: "white",
+    size: "medium"
+};
+function add(num1, num2) {
+    return num1 + num2;
+}
+function sayHello() {
+    console.log("hello");
+    return;
+}
+var tmp = null;
+console.log(sayHello());
+var anotheradd = function add(num1, num2) {
+    return num1 + num2;
+};
+anotheradd;
+var doubleNumber = function (num) { return num * 2; };
+function doubleAndHandler(num, cd) {
+    var doubleNum = cd(num * 2);
+    console.log(doubleNum);
+}
+doubleAndHandler(32, function (doubleNum) {
+    return doubleNum;
+});
+var nuknownInput;
+var anyInout;
+var text;
+nuknownInput = "hello";
+nuknownInput = 21;
+nuknownInput = true;
+text = anyInout;
+if (typeof nuknownInput === "string") {
+    text = nuknownInput;
+}
+function error(message) {
+    throw new Error(message);
+}
+console.log(error("thi is an error"));
--- index.ts
+++ index.ts
@@ -1,7 +1,120 @@
-import axios from "axios";
-let hello: string = "hello";
-console.log(hello);
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 3.14;
+let negative: number = -0.12;
+let single: string = "hello";
+let double: string = `hello`;
 
-function add(a: number, b: number): number {
-  return a + b;
+const person = {
+  name: {
+    first: "Jack",
+    last: "Swith"
+  },
+  age: 21
+};
+
+const fruits: string[] = ["Apple", "Banana", "Grape"];
+
+const book: [string, number, boolean] = ["business", 1500, false];
+
+enum CoffeeSize {
+  SHORT,
+  TALL,
+  GRANDE,
+  VENTI
 }
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL
+};
+
+coffee.size = CoffeeSize.TALL;
+
+console.log(CoffeeSize.TALL);
+
+let anything: any = true;
+anything = "hello ";
+anything = ["hello", 33, true];
+anything = {};
+anything.fifififi = "fufurfhr";
+let banana = "banana";
+banana = anything;
+
+let unionType: number | string = 10;
+unionType = "hello";
+unionType.toUpperCase();
+let unionTypes: (number | string)[] = [21, "hello"];
+
+const apple = "apple";
+
+type ClothSize = "small" | "medium" | "large";
+let clothSize: ClothSize = "medium";
+const cloth: {
+  color: string;
+  size: ClothSize;
+} = {
+  color: "white",
+  size: "medium"
+};
+
+cloth.size = "large";
+
+const cloths: {
+  color: string;
+  size: "small" | "medium" | "large";
+} = {
+  color: "white",
+  size: "medium"
+};
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+function sayHello(): void {
+  console.log("hello");
+  return;
+}
+let tmp: undefined = null;
+
+console.log(sayHello());
+
+const anotheradd: (n1: number, n2: number) => number = function add(
+  num1: number,
+  num2: number
+) {
+  return num1 + num2;
+};
+anotheradd;
+
+const doubleNumber: (num: number) => number = (num) => num * 2;
+
+function doubleAndHandler(num: number, cd: (num: number) => void): void {
+  const doubleNum = cd(num * 2);
+
+  console.log(doubleNum);
+}
+
+doubleAndHandler(32, (doubleNum) => {
+  return doubleNum;
+});
+
+let nuknownInput: unknown;
+let anyInout: any;
+let text: string;
+nuknownInput = "hello";
+nuknownInput = 21;
+nuknownInput = true;
+
+text = anyInout;
+
+if (typeof nuknownInput === "string") {
+  text = nuknownInput;
+}
+
+function error(message: string) {
+  throw new Error(message);
+}
+
+console.log(error("thi is an error"));
